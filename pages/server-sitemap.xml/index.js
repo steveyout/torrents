6e1be280b65abd2f2c38135760e124ff216d1308
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-03-03 20:03:41 +0300

    first commit

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,5 +1,6 @@
 import { getServerSideSitemap } from 'next-sitemap'
 import axios from "@/utils/axios";
+import { paramCase } from 'change-case';
 export const getServerSideProps = async (ctx) => {
   const response= await axios.get(`${process.env.API}/movies/12?sort=trending&order=-1`);
   const {data}=response
@@ -8,7 +9,7 @@ export const getServerSideProps = async (ctx) => {
     data.map((movie, index) =>{
     const{_id,title}=movie
     fields.push({
-      loc:  `${process.env.SITE_URL}/movie/${title}?imdb_id=${_id}`, // Absolute url
+      loc:  `${process.env.SITE_URL}/movie/${paramCase(title)}?imdb_id=${_id}`, // Absolute url
       lastmod: new Date().toISOString(),
       priority:1
     },)
