d3f7b17957b043da8ad3cf000653644cfeb324e4
youtsteve1@gmail.com <youtsteve1@gmail.com>
2024-04-29 21:59:56 +0300

    fixed some issues.

--- sections/movies/VideoPostTags.js
+++ sections/movies/VideoPostTags.js
@@ -24,8 +24,8 @@ export default function VideoPostTags({ post }) {
   if (episodes){
     torrents={
       en:{
-        '1080p':JSON.parse(JSON.stringify(episodes[0].torrents[0]).replace('seeds','seed').replace('peers','peer')),
-        '720p':JSON.parse(JSON.stringify(episodes[0].torrents['720p']||episodes[0].torrents['480p']).replace('seeds','seed').replace('peers','peer'))
+        '1080p':JSON.parse(JSON.stringify(episodes[0].torrents[0]).replace('seeds','seed').replace('peers','peer')||null),
+        '720p':JSON.parse(JSON.stringify(episodes[0].torrents['720p']||episodes[0].torrents['480p']).replace('seeds','seed').replace('peers','peer')||null)
       }
     }
     torrents.en['720p'].filesize='unknown'
@@ -59,15 +59,16 @@ export default function VideoPostTags({ post }) {
           <>
           <Stack direction={isDesktop?'row':'column'} alignItems={'center'} justifyContent={'space-between'} sx={{mb:5}}>
             <Box sx={{mb:3}}>
-              <Label variant={'filled'} color={'warning'}>{torrents.en['1080p'].seed} seeders</Label>
-              <Label variant={'filled'} color={'error'}>{torrents.en['1080p'].peer} leechers</Label>
-              <Label variant={'filled'} color={'secondary'}>{torrents.en['1080p'].filesize}</Label>
+              <Label variant={'filled'} color={'warning'} sx={{mr:2}}>{torrents.en['1080p'].seed} seeders</Label>
+              <Label variant={'filled'} color={'error'} sx={{mr:2}}>{torrents.en['1080p'].peer} leechers</Label>
+              <Label variant={'filled'} color={'secondary'} sx={{mr:2}}>{torrents.en['1080p'].filesize}</Label>
             </Box>
           <LoadingButton
             variant={'contained'}
             startIcon={<Iconify icon="mynaui:magnet" sx={{ width: 20, height: 20 }} />}
             onClick={ () => downloadTorrent( `/api/download?url=${torrents.en['1080p'].url}`, `${title}.torrent`)}
             loading={loading}
+            disabled={torrents.en['1080p']==null}
             fullWidth={!isDesktop}
           >
             1080p
@@ -75,10 +76,10 @@ export default function VideoPostTags({ post }) {
           </Stack>
 
           <Stack direction={isDesktop?'row':'column'} alignItems={'center'} justifyContent={'space-between'}>
-            <Box sx={{mb:3}}>
-              <Label variant={'filled'} color={'warning'}>{torrents.en['720p'].seed} seeders</Label>
-              <Label variant={'filled'} color={'error'}>{torrents.en['720p'].peer} leechers</Label>
-              <Label variant={'filled'} color={'secondary'}>{torrents.en['720p'].filesize}</Label>
+            <Box sx={{mb:3,}}>
+              <Label variant={'filled'} color={'warning'} sx={{mr:2}}>{torrents.en['720p']?torrents.en['720p'].seed:'none'} seeders</Label>
+              <Label variant={'filled'} color={'error'} sx={{mr:2}}>{torrents.en['720p']?torrents.en['720p'].peer:'none'} leechers</Label>
+              <Label variant={'filled'} color={'secondary'} sx={{mr:2}}>{torrents.en['720p']?torrents.en['720p'].filesize:'none'}</Label>
             </Box>
             <LoadingButton
               variant={'contained'}
@@ -87,6 +88,7 @@ export default function VideoPostTags({ post }) {
                 sx={{ width: 20, height: 20 }} />}
               onClick={ () => downloadTorrent( `/api/download?url=${torrents.en['720p'].url}`, `${title}.torrent`)}
               loading={loading}
+              disabled={torrents.en['720p']==null}
               fullWidth={!isDesktop}
             >
               720p
